4f85b1277333044bce63d7e784c1873d8bf1a0e5
glebkorotkov <ukmmm200@yandex.ru>
2024-06-07 02:26:39 +0700

    Choose Enemy or Spawn
    Choose

--- Assets/Scripts/PlayerUnit.cs
+++ Assets/Scripts/PlayerUnit.cs
@@ -18,6 +18,8 @@ namespace Spine.Unity
         public int attackEnemySecond = 10;
         private float damageTimer = 0.5f;
         private float damageInterval = 1.0f;
+        private string pointSpawn = "SpawnEnemy";
+        private string pointEnemy = "Enemy";
         
         UnityEngine.AI.NavMeshAgent agent;
 
@@ -27,15 +29,30 @@ namespace Spine.Unity
             agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
             agent.updateRotation = false;
             agent.updateUpAxis = false;
+            target = GameObject.FindWithTag(pointSpawn).transform;
         }
+
 
         private void Update()
         {
+            float deltaEnemyX = Mathf.Abs(unit.position.x - GameObject.FindWithTag(pointEnemy).transform.position.x);
+            float deltaEnemyY = Mathf.Abs(unit.position.y - GameObject.FindWithTag(pointEnemy).transform.position.y);
+            
+            float deltaSpawnX = Mathf.Abs(unit.position.x - GameObject.FindWithTag(pointSpawn).transform.position.x);
+            float deltaSpawnY = Mathf.Abs(unit.position.y - GameObject.FindWithTag(pointSpawn).transform.position.y);
+            
+            if ((deltaEnemyX * deltaEnemyX + deltaEnemyY * deltaEnemyY) >
+                (deltaSpawnX * deltaSpawnX + deltaSpawnY * deltaSpawnY))
+            {
+                target = GameObject.FindWithTag(pointSpawn).transform;
+            }
+            else
+            {
+                target = GameObject.FindWithTag(pointEnemy).transform;
+            }
             agent.SetDestination(target.position);
-
             float deltaX = Mathf.Abs(unit.position.x - target.position.x);
             float deltaY = Mathf.Abs(unit.position.y - target.position.y);
-
             if (deltaX < eps && deltaY < eps)
             {
                 playerAnim.AnimationName = "Attack";
