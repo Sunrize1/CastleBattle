44adedabb0b3f22100467104c00b96d432e4d4cf
Sunrize1 <vladislaw.2000.pro@gmail.com>
2024-06-06 22:31:09 +0700

    animation move idle edit

--- Assets/Scripts/PlayerUnit.cs
+++ Assets/Scripts/PlayerUnit.cs
@@ -9,11 +9,14 @@ namespace Spine.Unity
     {
         [SerializeField] SkeletonAnimation playerAnim;
         [SerializeField] Transform target;
+        Transform unit;
+        float eps = 2.0f;
 
         UnityEngine.AI.NavMeshAgent agent;
 
         private void Start()
         {
+            unit = GetComponent<Transform>();
             agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
             agent.updateRotation = false;
             agent.updateUpAxis = false;
@@ -22,7 +25,18 @@ namespace Spine.Unity
         private void Update()
         {
             agent.SetDestination(target.position);
-            playerAnim.AnimationName = "Move";
+
+            float deltaX = Mathf.Abs(unit.position.x - target.position.x);
+            float deltaY = Mathf.Abs(unit.position.y - target.position.y);
+
+            if (deltaX < eps && deltaY < eps)
+            {
+                playerAnim.AnimationName = "Idle";
+            }
+            else
+            {
+                playerAnim.AnimationName = "Move";
+            }
         }
     }
 }
